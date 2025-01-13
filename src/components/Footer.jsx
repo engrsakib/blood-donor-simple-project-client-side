32803fc581c2a41eaf1f4ed7dbb0b63a302cf82b
engrsakib <snazmussakib01@gmail.com>
2025-01-14 01:27:47 +0600

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      {/* <footer className="footer mx-auto text-base-content p-10">
+      <footer className="footer mx-auto text-base-content p-10">
         <nav>
           <h6 className="footer-title">Services</h6>
           <a className="link link-hover">Branding</a>
@@ -24,23 +24,8 @@ const Footer = () => {
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
-        <form>
-          <h6 className="footer-title">Newsletter</h6>
-          <fieldset className="form-control w-80">
-            <label className="label">
-              <span className="label-text">Enter your email address</span>
-            </label>
-            <div className="join">
-              <input
-                type="text"
-                placeholder="username@site.com"
-                className="input input-bordered join-item"
-              />
-              <button className="btn btn-primary join-item">Subscribe</button>
-            </div>
-          </fieldset>
-        </form>
-      </footer> */}
+        
+      </footer>
       <footer className="footer footer-center text-base-content p-4">
         <aside>
           <p>
