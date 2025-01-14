852f53daf2b038009814117c89bc9b4a013d1950
engrsakib <snazmussakib01@gmail.com>
2025-01-14 18:42:53 +0600

    admin initialize

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,52 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import { RiAdminFill } from "react-icons/ri";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
+import { Helmet } from "react-helmet";
+import { AuthContext } from "../../provider/AuthProvider";
+import { FaUserGraduate } from "react-icons/fa";
 
 const Dashboard = () => {
+  const {user, dark} = useContext(AuthContext);
+  // console.log(user)
+
+  const adminDashboard = (
+    <>
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) =>
+          `btn btn-outline btn-accent btn-wide ${isActive ? "btn-active" : ""}`
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/dashboard/all-users"
+        className={({ isActive }) =>
+          `btn btn-outline btn-accent btn-wide ${isActive ? "btn-active" : ""}`
+        }
+      >
+        All users
+      </NavLink>
+      <NavLink
+        to=" /dashboard/all-blood-donation-request"
+        className={({ isActive }) =>
+          `btn btn-outline btn-accent btn-wide ${isActive ? "btn-active" : ""}`
+        }
+      >
+        All Blood Donation Request
+      </NavLink>
+      <NavLink
+        to="/dashboard/content-management"
+        className={({ isActive }) =>
+          `btn btn-outline btn-accent btn-wide ${isActive ? "btn-active" : ""}`
+        }
+      >
+        Content Management
+      </NavLink>
+    </>
+  );
+
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -21,17 +66,53 @@ const Dashboard = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <ul className="menu bg-orange-800 rounded-lg text-base-content min-h-full w-80 p-4">
           {/* Sidebar content */}
-          <Link to={'/'}>
-            <h1 className="text-red-950 uppercase text-center text-2xl font-bold">
+          <Link to={"/"}>
+            <h1 className="text-slate-50 uppercase text-center text-2xl font-bold">
               BloodBridge
             </h1>
           </Link>
           <h3 className="text-xl text-center capitalize">dashboard</h3>
-          {/* admin Dashboard */}
+          {/* user icons and profile start */}
+          <section className="flex flex-col justify-center items-center mt-3">
+            <img
+              className="rounded-full border shadow h-[60px] w-[60px]"
+              src={user.photoUrl}
+              alt="userPhoto"
+            />
+            <h1 className="flex gap-x-1 items-center text-white text-lg">
+              {" "}
+              <FaUserGraduate /> {user.name}
+            </h1>
+            <h1 className="flex gap-x-1 items-center text-white text-sm">
+              {" "}
+              <MdOutlineDriveFileRenameOutline /> {user.role}
+            </h1>
+            <Link to={"/dashboard/profile"} className="btn btn-outline btn-info btn-wide mt-1">profile</Link>
+          </section>
+          {/* user icons and profile end */}
+
+          {/* admin Dashboard start*/}
+          <section className="mt-2">
+            {/* icons */}
+            <div className="flex justify-start items-center gap-3 text-white text-xl">
+              <RiAdminFill /> <h4>admin</h4>
+            </div>
+            {/* menue */}
+            <menu className="mt-1 flex flex-col gap-y-2 justify-center items-center">
+              {
+                adminDashboard
+              }
+            </menu>
+            {/* menue */}
+          </section>
+          {/* admin Dashboard end*/}
         </ul>
       </div>
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
     </div>
   );
 };
