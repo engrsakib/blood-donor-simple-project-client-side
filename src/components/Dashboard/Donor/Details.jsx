fb213424d3113c2a9cab391b5f1d95c39579dc10
engrsakib <snazmussakib01@gmail.com>
2025-01-16 17:41:54 +0600

    react modal init

--- src/components/Dashboard/Donor/Details.jsx
+++ src/components/Dashboard/Donor/Details.jsx
@@ -1,62 +1,58 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 import Swal from "sweetalert2";
 import { Helmet } from "react-helmet";
 import { AuthContext } from "../../../provider/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { cleanPath } from "@tanstack/react-router";
+import { GrStatusWarning } from "react-icons/gr";
 
 const Details = () => {
   const { dark, setActive, active } = useContext(AuthContext);
-  const data = useLoaderData();
+  const [disabled, setdisabled] = useState(false);
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
   // console.log(data[0])
+  const { id } = useParams();
+  // console.log(id)
   const {
-    _id,
-    name,
-    mail,
-    title,
-    photoURL,
-    type,
-    description,
-    moneyNedd,
-    minimumMoney,
-    deadline,
-  } = data[0];
-
-  // count down
-  const [timeLeft, setTimeLeft] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
+    isLoading: isPending,
+    data: data = [],
+    refetch,
+  } = useQuery({
+    queryKey: ["donations", id],
+    queryFn: async () => {
+      const response = await axios.get(
+        `http://localhost:5000/donations/edit/${id}`
+      );
+      return response.data;
+    },
   });
 
-  useEffect(() => {
-    const targetDate = new Date(`${deadline}T00:00:00`);
-
-    const calculateTimeLeft = () => {
-      const now = new Date();
-      const difference = targetDate - now;
-
-      if (difference > 0) {
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((difference / (1000 * 60)) % 60);
-        const seconds = Math.floor((difference / 1000) % 60);
-        setActive(true);
-        setTimeLeft({ days, hours, minutes, seconds });
-      } else {
-        setActive(false);
-        setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-      }
-    };
-
-    const interval = setInterval(calculateTimeLeft, 1000);
-
-    return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, [deadline, setActive]);
-
+  console.log(data);
+  const {
+    recipientName,
+    district,
+    upazila,
+    hospital,
+    address,
+    bloodGroup,
+    donationDate,
+    donationTime,
+    requestMessage,
+    requesterName,
+    _id,
+    email,
+    status,
+  } = data;
+  
+  useEffect(()=>{
+    if (status == "canceled" || status == "done" || status == "inprogress") {
+      setdisabled(true);
+    }
+  },[status])
   // donetation section handel
   const handleDonate = (id) => {
     if (active) {
@@ -83,52 +79,35 @@ const Details = () => {
         {/* Left Section */}
         <div className="flex-1">
           <img
-            src={photoURL}
+            src={`https://i.ibb.co.com/hDR0Z6f/friendly-hospital-phlebotomist-collecting-blood-sample-from-patient-lab-preparation-blood-test-by-fe.jpg`}
             alt="Fundraiser"
             className="rounded-lg shadow-md w-full h-[400px] object-cover"
           />
-          <h1 className="text-3xl font-bold mt-4">{title}</h1>
+          <h1 className="text-3xl font-bold mt-4">{}</h1>
           <p className="text-gray-600 mt-2">
-            <span className="font-semibold badge ">Types: {type} </span>
+            <span className="font-semibold badge ">
+              <GrStatusWarning /> {status}{" "}
+            </span>
           </p>
           <div
             className={`${
               active ? "bg-green-400" : "bg-red-400"
             } mt-4 p-4 rounded-md`}
-          >
-            <p className="text-sm font-medium">
-              <i className="fas fa-shield-alt mr-2 text-white">
-                {active ? "Active" : "Closed"}
-              </i>
-            </p>
-          </div>
+          ></div>
           <p className={`${dark ? "text-gray-200" : "text-gray-800"} mt-4`}>
-            {description}
+            {requestMessage}
           </p>
         </div>
 
         {/* Right Section */}
         <div className="w-full lg:w-1/3 bg-white p-6 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold">{moneyNedd} TK needed</h2>
+          <h2 className="text-2xl font-bold">{bloodGroup} needed</h2>
           <p className="text-gray-600">
-            Name: {name} <br /> Mail: {mail}
+            Name: {requesterName} <br /> Mail: {email}
           </p>
-          <div className="my-4">
-            <div className="relative h-4 w-full bg-gray-200 rounded-full">
-              <div
-                className="absolute top-0 left-0 h-4 bg-yellow-500 rounded-full"
-                style={{ width: "74%" }}
-              ></div>
-            </div>
-            <p className="text-sm text-gray-500 mt-1">74%</p>
-          </div>
-          <button className="btn btn-outline btn-accent w-full">
-            <p>
-              {timeLeft.days} days, {timeLeft.hours} hours, {timeLeft.minutes}{" "}
-              minutes, and {timeLeft.seconds} seconds
-            </p>
-          </button>
+
           <button
+            disabled={disabled}
             onClick={() => {
               handleDonate(_id);
             }}
@@ -140,8 +119,32 @@ const Details = () => {
           <h3 className="mt-6 text-lg font-semibold">Other Informations</h3>
           <ul className="mt-4 space-y-2">
             <li className="flex justify-between">
-              <p className="font-medium">Minimum Donations Amount</p>
-              <p className="text-gray-500">{minimumMoney} TK</p>
+              <p className="font-medium">Recipient Name</p>
+              <p className="text-gray-500">{recipientName}</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="font-medium">District</p>
+              <p className="text-gray-500">{district}</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="font-medium">Upazila</p>
+              <p className="text-gray-500">{upazila}</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="font-medium">Hospital</p>
+              <p className="text-gray-500">{hospital}</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="font-medium">Address</p>
+              <p className="text-gray-500">{address}</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="font-medium">Donation Date</p>
+              <p className="text-gray-500">{donationDate}</p>
+            </li>
+            <li className="flex justify-between">
+              <p className="font-medium">Donation Time</p>
+              <p className="text-gray-500">{donationTime}</p>
             </li>
             {/* <li className="flex justify-between">
               <p className="font-medium">Davinder Sapra</p>
